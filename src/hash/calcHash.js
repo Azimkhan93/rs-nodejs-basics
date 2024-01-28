eb0e8ef328d1b38b166e165207c1f6144cc5be98
Azimkhan93 <aabdulsatarov@gmail.com>
2024-01-29 03:31:15 +0500

    feat: src hash calcHash is done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+// Calculate a rolling hash.
+import { createReadStream } from "fs";
+import { createHash } from "node:crypto";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  const readStream = createReadStream(filePath);
+  const hash = createHash("sha256");
+  
+  readStream.on("data", (data) => {
+    hash.update(data);
+  });
+
+  readStream.on("end", () => {
+    const hashResult = hash.digest("hex");
+    console.log(`Hash result is ${hashResult}`);
+  });
+
+  readStream.on("error", (error) => {
+    console.error(`Error reading file: ${error.message}`);
+  });
 };
 
 await calculateHash();
