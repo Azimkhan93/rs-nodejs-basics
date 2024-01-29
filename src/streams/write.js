43871dee00cf1707c0fdaca0a4b75d7aec00e559
Azimkhan93 <aabdulsatarov@gmail.com>
2024-01-29 12:47:30 +0500

    feat: src streams write is done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import { createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+import { stdin } from "process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, "files", "fileToWrite.txt");
+  const writeStream = createWriteStream(filePath);
+
+  stdin.pipe(writeStream);
+
+  writeStream.on("error", (err) => {
+    console.error("Error reading the file:", err);
+  });
 };
 
 await write();
